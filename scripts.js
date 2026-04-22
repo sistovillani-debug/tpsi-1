b26a554f4a738585c831b5635142c60b19de6f36
sistovillani-debug <sisto.villani@isissmatese.edu.it>
2026-04-22 09:58:43 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,21 @@
+function loadNews() {
+//Istanzia un oggetto request
+ const xhr = new XMLHttpRequest();
+ //Definisce la risorsa (news.txt) da aprire sul server
+ // e il metodo http (get) da usare per l’invio della
+ richiesta
+ xhr.open("get", "news.txt");
+ //Definisce una funzione callback
+ //e effettua il controllo se la risposta è stata ricevuta
+ interamente
+ xhr.onload = function () {
+ if (xhr.readyState === xhr.DONE) {
+  if (xhr.status === 200) {
+   document.getElementById("news").innerHTML = xhr.
+   responseText;
+    }
+  }
+}
+//Invia la richiesta al server senza specificare dati (null)
+xhr.send(null);
+} 
